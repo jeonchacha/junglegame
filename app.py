15d9aa7a5cb39be8e98082c7b3452cb677e392ce
jeonseyoung <jsy9831@gmail.com>
2025-07-08 21:38:57 +0900

    commit 2

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ class CustomJSONEncoder(json.JSONEncoder):
             return str(o)
         return json.JSONEncoder.default(self, o)
 
-
 class CustomJSONProvider(JSONProvider):
     def dumps(self, obj, **kwargs):
         return json.dumps(obj, **kwargs, cls=CustomJSONEncoder)
@@ -26,6 +25,47 @@ app.json = CustomJSONProvider(app)
 @app.route('/')
 def home():
    return render_template('index.html')
+
+@app.route('/login', methods=['GET'])
+def login():
+    id = request.form['id']
+    pw = request.form['pw']
+
+    user = find_user(id, pw)
+
+    if user != None:
+        return jsonify({'result':'success', 'userinfo':user})    
+    else:
+        return jsonify({'result':'failure', 'msg':'아이디가 존재하지 않거나 비밀번호가 틀렸습니다.'})
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    name = request.form['name']
+    id = request.form['id']
+    pw = request.form['pw']
+    id_github = request.form['id_github']
+
+    userdoc = {
+        'name':name,
+        'id':id,
+        'pw':pw,
+        'id_github':id_github
+    }
+
+    create_user(userdoc)
+
+    return jsonify({'result':'success'})
+
+@app.route('/checkid', methods=['GET'])
+def checkid():
+
+    id = request.args.get('id')
+    user = find_id(id)
+
+    if user != None:
+        return jsonify({'result':'failure', 'msg':user['id'] + ' 는 중복된 아이디 입니다.'})
+    else:
+        return jsonify({'result':'success', 'msg':id + ' 는 사용가능한 아이디 입니다.'})
 
 if __name__ == '__main__':
    print(sys.executable)  
--- repository.py
+++ repository.py
@@ -2,4 +2,13 @@ from pymongo import MongoClient
 from bson import ObjectId
 
 client = MongoClient('localhost', 27017)
-db = client.jungle
+db = client.junglegame
+
+def create_user(userdoc):
+    db.user.insert_one(userdoc)
+
+def find_id(id):
+    return db.user.find_one({'id':id})
+
+def find_user(id, pw):
+    return db.user.find_one({'id':id, 'pw':pw})
